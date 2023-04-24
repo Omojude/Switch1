5111aae827c7fdcc3ec233ef37b037b2dab5ed15
Omojude <judeadoke4@gmail.com>
2023-04-24 14:19:14 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+function  determinePeriodOfTime(number){
+    switch(number){
+    case 10:
+    console.log('decade')
+    break;
+      
+    case 100:
+    console.log('century')
+    break;
+      
+    case 1000:
+    console.log('millienuim')
+    break
+    
+    default:
+    console.log('number does not match')
+    
+  }
+  }
+  determinePeriodOfTime(1000)
+  
